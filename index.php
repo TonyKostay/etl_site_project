55220f9f29ca5c5c3b7325e4d116b3f2714df26c
TonyKostay <tonykostay@yandex.ru>
2022-06-25 18:35:28 +0300

    header_done

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="src/css/style.min.css">
+    <script src="src/scripts/script.js"></script>
+    <title>ETL</title>
+</head>
+<body>
+    <header class="main-header">
+        <div class="main-header__wrapper wrapper">
+            <img class="main-header___logo" src="src/img/logo.svg">
+            <nav class="main-header__nav-bar">
+                <div class="main-header__nav-mobile">
+                    <img src="src/img/menu-mobile.svg" alt="">
+                </div>
+                <ul class="main-header__nav-bar_items">
+                    <li><img src="src/img/phone-icon.svg" id='phoneIcon'><a href="tel:+78142332211">+7 800-555-35-35</a></li>
+                    <li> <a href="">Преимущества</a></li>
+                    <li><a href="">О продукте</a></li>
+                    <li><a href="">Фото-портфолио</a></li>
+                    <li><a href=""><button class='main-header__nav-bar-button button'>Написать в WhatsApp</button></a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <section class="mainScreen">
+    </section>
+</body>
+</html>
