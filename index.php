e9e6f48888d0988996af1115939e867972f38a30
TonyKostay <tonykostay@yandex.ru>
2022-06-30 20:35:53 +0300

    secondScreenDone

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
             </nav>
         </div>
     </header>
-    <section class="mainScreen">
+    <div class="mainScreen">
         <div class="mainScreen__wrapper wrapper">
             <div class="mainScreen__wrapper-content">
                 <h1 class='mainScreen__header header'>Умная подсветка<br>лестницы Волгоград</h1>
@@ -34,16 +34,73 @@
                 <a href=""><button class='mainScreen__button button'>Написать в WhatsApp</button></a>
             </div>
         </div>
-    </section>
-    <!-- <section class="settings">
-        <div class="settings__wrapper wrapper">
-            <div class="settings__item">
-                <div class="setting__item-gradient">
-                    <span class="setting__item-subheader"></span>
-                    <p></p>
+    </div>
+    <section class="advantages">
+        <div class="advantages__wrapper wrapper">
+            <div class="advantages__item" id="interface">
+                <div class="advantages__item-desc">
+                    <h3 class="advantages__item-subheader">Интерфейс</h3 >
+                    <p>Основа системы, отвечает за настройки включения, работы и выключения подсветки. Задает время и интенсивность свечения</p>
+                    <ul>
+                        <li>Количество ступеней</li>
+                        <li>Яркость общая</li>
+                        <li>Яркость первой и последней ступеней</li>
+                        <li>Время розжига</li>
+                        <li>Время затухания</li>
+                        <li>Ожидание подъема человека </li>
+                        <li>Длина первой ступени </li>
+                        <li>Длина последней ступени </li>
+                        <li>увствительность датчика</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="advantages__item">
+                <div class="advantages__item-image" id="item-2"></div>
+                <div class="advantages__item-desc">
+                    <h3 class="advantages__item-subheader">Легкая настройка</h3>
+                    <p>Настраивайте подсветку под себя с помощью любого смартфона — просто подключитесь к контроллеру по Wi-Fi</p>
+                </div>
+            </div>
+            <div class="advantages__item">
+                <div class="advantages__item-image" id="item-3"></div>
+                <div class="advantages__item-desc">
+                    <h3 class="advantages__item-subheader">Безопасность</h3>
+                    <p>Система защищена от короткого замыкания, а все кабели и контроллер спрятаны вглубь лестницы от детей или домашних животных</p>
+                </div>
+            </div>
+            <div class="advantages__item" id='instruction'>
+                <div class="advantages__item-desc">
+                    <h3 class="advantages__item-subheader">Инструкция</h3>
+                    <p>Основа системы, отвечает за настройки включения, работы и выключения подсветки. Задает время и интенсивность свечения</p>
+                    <a href=""><button class='advantages__item-button button'>Скачать инструкцию</button></a>
+                </div>
+            </div>
+            <div class="advantages__item">
+                <div class="advantages__item-image" id="item-5"></div>
+                <div class="advantages__item-desc">
+                    <h3 class="advantages__item-subheader">Автономная работа</h3>
+                    <p>Подсветка загорается автоматически при вашем приближении, ее не нужно включать или выключать</p>
+                </div>
+            </div>
+            <div class="advantages__item">
+                <div class="advantages__item-image" id="item-6"></div>
+                <div class="advantages__item-desc">
+                    <h3 class="advantages__item-subheader">Монтаж за 1 час</h3>
+                    <p>Подсветка ETL - это конструктор, который легко монтируется даже на готовую лестницу. Легко, безопасно и эстетично</p>
                 </div>
             </div>
         </div>
-    </section> -->
+    </section>
+    <section>
+        <div class="video-container">
+        <video autoplay muted loop>
+            <source src="" type="video/mp4" />
+        </video>
+        <div class="caption">
+        <h2>Your caption here</h2>
+        </div>
+</div>
+    
+    </section>
 </body>
 </html>
